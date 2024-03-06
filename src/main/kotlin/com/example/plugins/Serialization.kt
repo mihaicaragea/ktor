05cfc4d9d8234a7314c9bf5b11354f66d6c71c4f
carageamihai <mihai.caragea@mastercard.com>
2024-03-06 15:55:05 +0200

    first commit

---
+++ src/main/kotlin/com/example/plugins/Serialization.kt
@@ -0,0 +1,27 @@
+package com.example.plugins
+
+import com.fasterxml.jackson.databind.*
+import io.ktor.serialization.gson.*
+import io.ktor.serialization.jackson.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureSerialization() {
+  install(ContentNegotiation) {
+    gson {
+    }
+    jackson {
+      enable(SerializationFeature.INDENT_OUTPUT)
+    }
+  }
+  routing {
+    get("/json/gson") {
+      call.respond(mapOf("hello" to "world"))
+    }
+    get("/json/jackson") {
+      call.respond(mapOf("hello" to "world"))
+    }
+  }
+}
